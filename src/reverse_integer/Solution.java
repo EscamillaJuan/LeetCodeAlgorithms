7f6f88fb256c090918b3470bd8e6040f31a90876
EscamillaJuan <juanpedroescamilla@outlook.com>
2025-01-09 21:06:31 -0600

    add reverse_integer Olog(n)

--- src/reverse_integer/Solution.java
+++ src/reverse_integer/Solution.java
@@ -2,6 +2,23 @@ package reverse_integer;
 
 public class Solution {
     public int reverse(int x) {
-        return 1;
+        int digit = 0;
+        long reverse = 0;
+        while (x != 0) {
+            digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) reverse;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.reverse(123));
     }
 }
